e7832b7e1aac86372bf53918981a8a8c686c5dc9
nemo-13 <nik.ogorodnikov.00@yandex.ru>
2025-02-26 11:52:03 +0300

    first commit

---
+++ NoHeretics/src/main/java/org/mephi/b23902/groupc/noheretics/NoHeretics.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.mephi.b23902.groupc.noheretics;
+
+/**
+ *
+ * @author Nikolas
+ */
+public class NoHeretics {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ NoHeretics/src/main/java/org/mephi/b23902/groupc/noheretics/View.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.mephi.b23902.groupc.noheretics;
+
+/**
+ *
+ * @author Nikolas
+ */
+public class View {
+    
+}
